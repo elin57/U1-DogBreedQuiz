bab0169091400fadf96dfdaa323ee7d6cc45e066
elin57 <112911997+elin57@users.noreply.github.com>
2023-02-18 03:03:57 +0100

    Gjorde klart login- och registrera-sidorna
    -Gjorde funktionen switchPage() som ändrar sidan från att registrera sig till att logga in och tvärtom.
    -Gjorde en request för att logga in sig som förbereds enligt input-fälten på den sidan.
    -Gjorde så att ett felmeddelande visas när man loggar in sig och något blir fel, och ändrade den paragrafen beroende på vilket error. Skrev alltså mer kod i funktionen statusUpdate().

--- kod/js/login_register.js
+++ kod/js/login_register.js
@@ -1,19 +1,26 @@
-"use strict";
 
 let userPrefix = "https://teaching.maumt.se/apis/access/";
 
-document.querySelector("#registerButton").addEventListener("click", defineRequest);
+document.querySelector("#mainContent > button:nth-of-type(1)").addEventListener("click", defineRequest);
 
-function defineRequest(event) {
-    if(event.currentTarget.id === "registerButton") {
-        prepareRequest("post");
-    } else if(event.currentTarget.id === "loginButton") {
-        prepareRequest("get");
+function defineRequest() {
+    if(document.querySelector("#firstContainer").classList.contains("registerPage")) {
+        prepareRequest("register");
+    } else if(document.querySelector("#firstContainer").classList.contains("loginPage")) {
+        prepareRequest("login");
     }
+    
+    /*if(event.currentTarget.id === "registerButton") {
+        prepareRequest("register");
+    } else if(event.currentTarget.id === "loginButton") {
+        prepareRequest("login");
+    }*/
 }
 
 function prepareRequest(postOrGet) {
-    if(postOrGet === "post") {
+
+    
+    if(postOrGet === "register") {
         let username = document.querySelector("input#username").value;
         console.log(username);
         let password = document.querySelector("input#password").value;
@@ -28,8 +35,19 @@ function prepareRequest(postOrGet) {
                 password: password
             })
         });
+        startFetch(postRequest);
+
     
-        getResource(postRequest);
+    
+    } else if(postOrGet === "login") {
+        let username = document.querySelector("input#username").value;
+        let password = document.querySelector("input#password").value;
+        console.log(username);
+        console.log(password);
+
+        let getRequest = new Request(`${userPrefix}?action=check_credentials&user_name=${username}&password=${password}`);
+        //prefix?action=check_credentials&user_name=X&password=Y
+        startFetch(getRequest);
     }
 
     if(document.querySelector("#showStatus") === null) {
@@ -64,6 +82,13 @@ function statusUpdate(status) {
             <button id='closePopup'>CLOSE</button>
             </div>
             `;
+        } else if(status === 400) {
+            popup.innerHTML = `
+            <div id=contentContainer>
+            <p>One or both of the slots weren't filled in.</p>
+            <button id='closePopup'>CLOSE</button>
+            </div>
+            `;
         } else {
             popup.innerHTML = `
             <div id=contentContainer>
@@ -72,12 +97,72 @@ function statusUpdate(status) {
             </div>
             `;
         }
+        document.querySelector("#closePopup").addEventListener("click", closePopup);
+
+    } else if(document.querySelector("#firstContainer").classList.contains("loginPage")) {
+        document.querySelector("#showStatus").style.visibility = "collapse";
+        if(status === 404) {
+            document.querySelector("#mainContent > p").textContent = "Wrong user name or password.";
+            document.querySelector("#mainContent > p").style.backgroundColor = "white";
+        } else if(status === 400) {
+            document.querySelector("#mainContent > p").textContent = "One or both of the slots weren't filled in.";
+            document.querySelector("#mainContent > p").style.backgroundColor = "white";
+        } else if(status === 418) {
+            document.querySelector("#showStatus").style.visibility = "visible";
+            popup.innerHTML = `
+            <div id=contentContainer>
+            <p>The server thinks it's not a teapot!</p>
+            <button id='closePopup'>CLOSE</button>
+            </div>
+            `;
+            document.querySelector("#closePopup").addEventListener("click", closePopup);
+        } else {
+            prepareQuiz();
+        }
     }
 
-    document.querySelector("#closePopup").addEventListener("click", closePopup);
     function closePopup() {
         document.querySelector("#showStatus").style.visibility = "collapse";
     }
 
 }
 
+document.querySelector("#mainContent > div:last-child > span").addEventListener("click", switchPage);
+
+function switchPage() {
+
+    if(document.querySelector("#firstContainer").classList.contains("registerPage")) {
+
+        document.querySelector("#firstContainer").classList.remove("registerPage");
+        document.querySelector("#firstContainer").classList.add("loginPage");
+    
+        document.querySelector("h1:nth-of-type(1)").textContent = "LOGIN";
+        document.querySelector("p:nth-of-type(1)").textContent = "Let the magic start!";
+        document.querySelector("#mainContent > p").style.backgroundColor = "rgb(94, 225, 181)";
+
+        document.querySelector("#mainContent > button:nth-of-type(1)").textContent = "Login";
+        document.querySelector("#mainContent > div:last-child > span").textContent = "New to this? Register for free";
+        
+        document.querySelector("#firstContainer").style.backgroundColor = "rgb(94, 225, 181)";
+        document.querySelector("#switchPage").style.backgroundColor = "rgb(94, 225, 181)";
+        
+        /*document.querySelector("#mainContent > button:nth-of-type(1)").removeAttribute("id");
+        document.querySelector("#mainContent > button:nth-of-type(1)").setAttribute("id", "loginButton");*/
+
+    } else {
+        document.querySelector("#firstContainer").classList.remove("loginPage");
+        document.querySelector("#firstContainer").classList.add("registerPage");
+        document.querySelector("#firstContainer").style.backgroundColor = "rgb(7, 130, 73)";
+        document.querySelector("#switchPage").style.backgroundColor = "rgb(7, 130, 73)";
+
+        document.querySelector("h1:nth-of-type(1)").textContent = "REGISTER";
+        document.querySelector("p:nth-of-type(1)").textContent = "Ready when you are...";
+        document.querySelector("#mainContent > p").style.backgroundColor = "rgb(7, 130, 73)";
+
+        document.querySelector("#mainContent > button:nth-of-type(1)").textContent = "Register";
+        document.querySelector("#mainContent > div:last-child > span").textContent = "Already have an account? Go to login";
+
+        /*document.querySelector("#mainContent > button:nth-of-type(1)").removeAttribute("id");
+        document.querySelector("#mainContent > button:nth-of-type(1)").setAttribute("id", "registerButton");*/
+    }
+}
--- kod/js/server_connection.js
+++ kod/js/server_connection.js
@@ -1,9 +1,25 @@
-async function getResource(request) {
-    let response = await fetch(request);
+"use strict";
+async function startFetch(request) {
+    const response = await fetch(request);
     console.log(response);
     statusUpdate(response.status);
-    let resource = await response.json();
+
+    const resource = await response.json();
     console.log(resource);
+    return resource;
+    
 }
+
+/*async function getResource(whatRequest) {
+    let resource = await startFetch(whatRequest);
+    console.log(resource);
+}*/
+
+
+/*async function start_up(request) {
+    const n1 = await getResource(request);
+    console.log(n1);
+ }*/
+ 
 
 //All kommunikation med servrarna måste lösas med async och await. Metoden .then får inte förekomma i kodbasen.
