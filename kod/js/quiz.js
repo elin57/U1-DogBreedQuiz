3c582b1b6c7f8be44568fbd57edc1d8e45a7d073
elin57 <112911997+elin57@users.noreply.github.com>
2023-02-19 15:07:53 +0100

    Har gjort HTML och CSS till quiz-sidan och en array med 4 namn
    -När man loggar in med rätt värden så fetchas en hundbild.
    -I funktionen placePictureAndOptions() skapas en array med namn-nyckeln på rätt index för hundbilden, och tre andra namn-nycklar från random objekt i arrayen.
    -När arrayen dogNames har 4 element sorteras den så att rätt namn inte alltid hamnar i samma div-element.
    -Sedan placeras namnen i denna arrayen i varsitt div-element i containern #optionsContainer under hundbilden.

--- kod/js/quiz.js
+++ kod/js/quiz.js
@@ -2,27 +2,114 @@
 
 function get_random_number (max, min = 0) {
     // Returns a random number between min (inclusive) and max (exclusive)
-    return min + Math.floor( max * Math.random() );
-  }
+    return min + Math.floor(max * Math.random());
+}
 
 function prepareQuiz() {
     document.querySelector("#firstContainer").classList.remove("loginPage");
     //Kommer ha endless loop om jag inte tar bort denna classen.
+
+    document.querySelector("#firstContainer").style.backgroundColor = "lightblue";
+    document.querySelector("header").innerHTML = `
+    <div id="top">
+        <img src="media/logo.png">
+        <div>Dog Breed Quiz</div>
+    </div>
+    <div id=toLogout>
+        <p id=username></p>
+        <button>logout</button>
+    </div>
+    `;
+    document.querySelector("#mainContent").innerHTML = `
+        <img id="dogPicture" src="media/logo.png">
+        <div id=optionsContainer>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    `;
+
+    
     if(!document.querySelector("#firstContainer").classList.contains("loginPage")) {
-        
-        getPicture();
         document.querySelector("#firstContainer").classList.add("quizPage");
+        getPicture();
     }
 
     return;
 }
 
-function getPicture() {
+async function getPicture() {
     let randomIndex = get_random_number(ALL_BREEDS.length, 0);
-    let elementURL = ALL_BREEDS[randomIndex].url;
-    console.log(elementURL);
+    let rightIndex = ALL_BREEDS[randomIndex];
+    console.log(rightIndex);
+    let rightOptionURL = rightIndex.url;
 
-    let dogPicture = `https://dog.ceo/api/breed/${elementURL}/images/random`;
-    startFetch(dogPicture);
+    let dogPicture = `https://dog.ceo/api/breed/${rightOptionURL}/images/random`;
+
+    let resource = await startFetch(dogPicture);
+
+
+    placePictureAndOptions(rightIndex, resource);
+
     return;
+}
+
+function placePictureAndOptions(rightIndex, resource) {
+
+    let dogNames = [];
+    dogNames.push(rightIndex.name);
+    console.log(dogNames);
+
+
+    checkIfSame();
+
+    function checkIfSame() {
+        while(dogNames.length < 4) {
+            let randomIndex = get_random_number(ALL_BREEDS.length, 0);
+            let dogName = ALL_BREEDS[randomIndex].name;
+        
+            if(dogNames.some(element => element === dogName)) {
+                continue;
+            }
+            else {
+                dogNames.push(dogName);
+                console.log(dogNames);
+            }
+        }
+        
+        shuffleNames();
+    }
+
+    function shuffleNames() {
+        dogNames.sort(comparisonFunction);
+
+        function comparisonFunction(element1, element2) {
+            if(element1.length < element2.length) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+
+        console.log(dogNames);
+    }
+
+
+    let nameOptions = document.querySelectorAll("#optionsContainer > div");
+    for(let i = 0; i < nameOptions.length; i++) {
+        nameOptions[i].textContent = dogNames[i];
+        nameOptions[i].addEventListener("click", rightOrWrong);
+    }
+        
+    
+
+    document.querySelector("#dogPicture").removeAttribute("src");
+    document.querySelector("#dogPicture").setAttribute("src", `${resource.message}`);
+
+    return rightIndex;
+}
+
+function rightOrWrong(event) {
+
 }
